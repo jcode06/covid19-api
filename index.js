1b22b1b352992744289776d103df148626bc655f
John Pangilinan <john.pangilinan1@gmail.com>
2020-05-05 21:57:14 -0700

    Added rate limiter to API

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
 const cors = require('cors');
 const compression = require('compression');
 const bodyParser = require('body-parser');
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 8080
 
 const app = express();
 app.use(bodyParser.json() );
+app.use( rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 200 // limit each IP to 100 reqs per windowMs
+}) );
 
 app.use('/api', cors(), routes);
 app.use('/', cors(), (req, res) => {
